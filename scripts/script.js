d086dc4af69dab304b1fa8cf9bd15e64a9a1f408
Rakibjrb <rakibjrb572157@gmail.com>
2023-08-28 23:58:38 +0600

    show all btn worked successfully

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,8 @@ const cardContainer = document.getElementById("cardContainer"),
   showAllBtnDiv = document.getElementById("showAllBtnDiv");
 
 //loading spinner function
-const isLoading = (trueOrFalse) => {
-  if (trueOrFalse) {
+const loadingHandler = (isLoading) => {
+  if (isLoading) {
     loadingDiv.classList.remove("hidden");
   } else {
     loadingDiv.classList.add("hidden");
@@ -14,40 +14,55 @@ const isLoading = (trueOrFalse) => {
 };
 
 //data fetch function
-const loadData = async (url = "iphone") => {
-  isLoading(true);
+const loadData = async (url = "13", isShowAll) => {
+  loadingHandler(true);
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${url}`
   );
   const data = await res.json();
-  setDynamic(data.data);
-  console.log(url);
+  setDynamic(data.data, isShowAll);
 };
 loadData();
 
 //dynamic card set part
-const setDynamic = (allPhones) => {
+const setDynamic = (phones, isShowAll) => {
   cardContainer.textContent = "";
-  phones = allPhones.slice(0);
-  phones.map((phone) => {
+
+  if (phones.length > 8 && !isShowAll) {
+    showAllBtnDiv.classList.remove("hidden");
+  } else {
+    showAllBtnDiv.classList.add("hidden");
+  }
+
+  if (!isShowAll) {
+    phones = phones.slice(0, 8);
+  }
+
+  phones.forEach((phone) => {
     const div = document.createElement("div");
     div.innerHTML = `<div id="${phone.slug}" class="card bg-base-100 shadow-xl">
-                  <figure class="px-10 pt-10">
-                    <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
-                  </figure>
-                  <div class="card-body items-center text-center">
-                    <h2 class="card-title mb-3">${phone.phone_name}</h2>
-                    <div class="card-actions">
-                      <button class="btn btn-primary">Show Details</button>
+                    <figure class="px-10 pt-10">
+                      <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                      <h2 class="card-title mb-3">${phone.phone_name}</h2>
+                      <div class="card-actions">
+                        <button class="btn btn-primary">Show Details</button>
+                      </div>
                     </div>
-                  </div>
-                </div>`;
+                  </div>`;
     cardContainer.appendChild(div);
   });
-  isLoading(false);
+  loadingHandler(false);
 };
 
-const searchHandler = () => {
+//search handler function
+const searchHandler = (isShowAll) => {
   const searchText = searchInput.value;
-  loadData(searchText);
+  loadData(searchText, isShowAll);
+};
+
+//show all btn handler function
+const showAllBtnHandler = () => {
+  searchHandler(true);
 };
